c8bac7eed39b75eb3db553c17b33eadc6c1705d0
gary-dang <35976926+gary-dang@users.noreply.github.com>
2018-03-02 14:17:45 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
 		
 		for(int i=1;i<=10;i++){
 			
-			System.out.println("Hello World #2 (updated this line on local)..."+i);
+			System.out.println("Hello World #2 (updated this line on gihub 2:17)..."+i);
 		}
 	}
 	
