b57969e95f20a19367b842b0e981af1e2c6cd1be
gary-dang <gary.dang@accenture.com>
2018-03-09 08:34:50 -0600

    local change 03092018 8:34

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
 	public static void main(String[] args){
 		
 		for(int i=1;i<=10;i++){
-			System.out.println("Hello World 2.......updated on GitHub"+i);
+			System.out.println("Hello World 2.......updated on GitHub - local change - 03092018 08:28"+i);
 		}
 	}
 	
